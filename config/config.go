cf1a492df27e57ad657bbb0b6ec2496fc981c911
MikeMwita <mwitamike8@gmail.com>
2024-03-26 13:37:47 +0300

    Setup app config

--- config/config.go
+++ config/config.go
@@ -1,4 +1,21 @@
 package config
 
+import "net/http"
+
+const (
+	APIUrl      string = "https://api.whatsapp-cloud.com/v1"
+	APIEndpoint string = "api"
+	ApiVersion         = "v1"
+)
+
+type Config struct {
+	HttpClient        *http.Client
+	Url               *string
+	ApiVersion        string
+	AccessToken       string
+	PhoneNumberID     string
+	BusinessAccountID string
+}
+
 type Credentials struct {
 }
