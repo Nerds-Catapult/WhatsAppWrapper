6ddf11697fb750d17a7c86266910a5dafc43efbe
MikeMwita <mwitamike8@gmail.com>
2024-03-26 13:37:20 +0300

    Add whatsapp data structure

--- api.go
+++ api.go
@@ -1,1 +1,26 @@
 package whatsAppCloud_go
+
+import (
+	"context"
+	"net/http"
+)
+
+type AuthCredentials struct {
+	AccessToken   string
+	PhoneNumberID string
+	BusinessID    string
+}
+
+type Message struct {
+	ID      string
+	Content interface{}
+}
+
+type Analytics struct {
+	Metrics map[string]interface{}
+}
+
+type (
+	RequestHook  func(ctx context.Context, request *http.Request) error
+	ResponseHook func(ctx context.Context, response *http.Response) error
+)
